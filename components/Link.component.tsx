713f194abc2f67958a6ed84c6cd878e6a7037588
Turbobot <turbobot@vercel.com>
2025-11-13 17:05:04 +0200

    feat(create-turbo): apply official-starter transform

--- components/Link.component.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import NextLink, { LinkProps } from "next/link";
-
-export function Link({ children, ...rest }: LinkProps & { children?: React.ReactNode }) {
-    return <NextLink  className="px-[2rem] text-[1.5rem] hover:text-moonstone hover:drop-shadow-moonstone" {...rest}>{children}</NextLink>
-}
